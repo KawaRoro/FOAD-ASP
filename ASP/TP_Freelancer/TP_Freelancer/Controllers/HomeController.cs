c2282daf8568f0db5b35fcf1b677a487b88957d6
Schoenmaeker <webmaster.benjamin@gmail.com>
2021-01-06 15:25:40 +0100

    #BS: Update E432-27

---
+++ ASP/TP_Freelancer/TP_Freelancer/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP_Freelancer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /*public string Index()
+        {
+            return "Hello world";
+        }*/
+
+
+    }
+}
