6418887b4370984ed1dece28fa042371938b779e
Schoenmaeker <webmaster.benjamin@gmail.com>
2021-01-14 10:15:46 +0100

    BS: Update Lite Chat

---
+++ ASP/LiteChat/LiteChatWebsite/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiteChatWebsite.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username != null && password != null && username.Equals("Ben") && password.Equals("123"))
+            {
+                //HttpContext.Session.SetString("username", username);
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.error = "Invalid Account";
+                return View("Index");
+            }
+        }
+
+        [Route("logout")]
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            //HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+    /*public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }*/
+}
---
+++ ASP/LiteChat/dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿    CREATE TABLE [dbo].[Users](  
+        [User-Id] [int] IDENTITY(1,1) NOT NULL, 
+        [User-Login] [nvarchar](50) NULL,   
+        [User-Password] [nvarchar](50) NULL,  
+        [User-Created] [datetime] NULL,  
+     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED   
+    (  
+        [User-Id] ASC  
+    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]  
+    ) ON [PRIMARY]  
+      
+    GO  
